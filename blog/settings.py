40ee6f158ea1b7dd7897e0c4d6db7cd2fe1a4e16
nibin-joseph05 <nibin.joseph.career@gmail.com>
2025-03-24 16:29:38 +0530

    third commit

--- blog/settings.py
+++ blog/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-s105(bv%-+vfnh@dp057ji*)o0xq%(u&of1dh$a(k_x5)hll4-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
